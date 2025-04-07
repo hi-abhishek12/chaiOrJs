2dcccd6588c0d2ae5f4c933a7dc21569889d4795
hi-abhishek12 <abhisahni2005@gmail.com>
2025-04-07 14:11:01 +0000

    prototype in js

---
+++ prototype.js
@@ -0,0 +1,35 @@
+// function multiply(num){
+//     return num*5;
+// }
+
+// const ans = multiply(5);
+// const ans2 = multiply.power = 2;
+// console.log(ans);
+// console.log(ans2);
+// console.log(multiply.prototype);
+
+// new keyword
+// function createUser (name , score){
+//     this.name = name,
+//     this.score = score
+// }
+
+// createUser.prototype.increment = function(){
+//     this.score++;
+// }
+// createUser.prototype.printMe = function(){
+//     console.log(`price of tea is ${this.score}`);
+// }
+// const first = new createUser('abhi', 12);
+// const second = new createUser('abhi', 10);
+
+// console.log(first)
+// console.log(second)
+// first.increment();
+// second.printMe();
+
+//prototype
+
+let name = 'abhishek   ';
+
+console.log(name.length);
